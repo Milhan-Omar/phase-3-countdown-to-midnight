e38755229f0a42c8f7659daf9d84f898a9a15a23
Milhan Osman <milhanomarosman@gmail.com>
2023-05-16 17:47:17 +0300

    completed assigment

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,15 @@
 #write your code here
+ 
 
-def countdown
+def countdown (count) 
+  while count > 0
+   puts "#{count} SECOND(S)!"
+    count -= 1
+    end
+    "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep (count)
+  sleep (count)
+  countdown(count)  
 end
