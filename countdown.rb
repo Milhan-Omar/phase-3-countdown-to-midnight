dade1b12affbc0e6b83a86c62a4222440ff3b6c3
Milhan Osman <milhanomarosman@gmail.com>
2023-05-17 14:23:24 +0300

    completed task

--- countdown.rb
+++ countdown.rb
@@ -2,14 +2,31 @@
  
 
 def countdown (count) 
+
   while count > 0
-   puts "#{count} SECOND(S)!"
+    puts "#{count} SECOND(S)!"
     count -= 1
-    end
-    "HAPPY NEW YEAR!"
+    sleep(1)
+  end
+  # for a in 0..count do
+  #   puts "#{a} SECOND(S)!"
+  #   sleep(1) 
+  # end
+
+  "HAPPY NEW YEAR!"
 end
 
 def countdown_with_sleep (count)
-  sleep (count)
-  countdown(count)  
+ puts countdown(count)  
 end
+
+countdown_with_sleep(5)
+
+
+# def looping(start, last)
+#     for a in start..last do
+#       puts a  
+#     end
+# end
+
+# looping(0, 100)
